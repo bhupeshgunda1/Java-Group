0a91c78b6edb66401335d575c3022c260d7b2e76
avkour13 <86690230+ManvirSodhi@users.noreply.github.com>
2022-01-27 18:49:22 -0500

    comment

--- Assignment1_Group3/src/main/java/com/st/ak/ab/bg/ms/week3/Assignment1/Compute.java
+++ Assignment1_Group3/src/main/java/com/st/ak/ab/bg/ms/week3/Assignment1/Compute.java
@@ -34,7 +34,7 @@ public class Compute extends HttpServlet {
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	 ***/
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
 		response.setContentType("text/html");  
